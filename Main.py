b5004b30db5b48f34035897509dd9000cec2b1e2
Volyando Belvadra <belgendo@gmail.com>
2021-05-21 23:51:13 +0700

    Implemented precision, accuracy, and recall
    I think the function is already pretty good.

--- Main.py
+++ Main.py
@@ -56,3 +56,40 @@ model.fit(X_train  , y_train)
 prediction = model.predict(X_test)
 
 #TODO : Hitung akurasi per kelas
+
+def generate_confusion_matrix(classes, predicted, actual):
+    #asumsi len predicted = len actual
+    # row = predicted, column = actual
+    result_dict = {i: {j: 0 for j in classes} for i in classes}
+    for i in range(len(predicted)):
+        result_dict[predicted[i]][actual[i]] += 1
+    return result_dict
+
+def calculate_precision(predict_class, matrix):
+    return matrix[predict_class][predict_class] / sum([matrix[predict_class][i] for i in matrix.keys()])
+
+def calculate_recall(predict_class, matrix):
+    return matrix[predict_class][predict_class] / sum([matrix[i][predict_class] for i in matrix.keys()])
+
+def calculate_accuracy(matrix):
+    correct = sum([matrix[i][i] for i in matrix.keys()])
+    total = sum([matrix[i][j] for i in matrix.keys() for j in matrix[i].keys()])
+    return correct / total
+    
+def pretty_print_matrix(matrix):
+    first_line = ""
+    first_line += "{:5.5}".format("     ")
+    for i in matrix.keys():
+        first_line += " {:5.5}".format(i)
+    rest = []
+    for i in matrix.keys():
+        curr_line = "{:5.5}".format(i)
+        for j in matrix[i].keys():
+            curr_line += " {:5.5}".format(str(matrix[i][j]))
+        rest.append(curr_line)
+    print(first_line)
+    for i in rest:
+        print(i)
+
+my_matrix = generate_confusion_matrix(model.predictor_class, prediction, y_test)
+
